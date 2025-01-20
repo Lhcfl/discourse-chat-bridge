e8105763f0029441a8ef754117cc94afc27db967
Lhc_fl <lhcfl@outlook.com>
2025-01-20 21:42:32 +0800

    format

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-ChatBridgeModule::Engine.routes.draw do
-  post "telegram/hook/:token" => "telegram_webhook#hook"
-end
+ChatBridgeModule::Engine.routes.draw { post "telegram/hook/:token" => "telegram_webhook#hook" }
 
 Discourse::Application.routes.draw { mount ::ChatBridgeModule::Engine, at: "chat-bridge" }
